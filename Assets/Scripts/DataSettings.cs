8215616bfa78929b0160e6206d1e60bb52e99506
ChopWoodFish <cxx301320@gmail.com>
2025-03-22 22:10:01 +0800

    feat: 游戏时间节奏开放参数控制

--- Assets/Scripts/DataSettings.cs
+++ Assets/Scripts/DataSettings.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class DataSettings : ScriptableObject
 {
     public List<Color> listDebrisColor = new List<Color>();
+
+    public float GenerateShapeDuration = 0.5f;
+    public int GenerateShapeCount = 8;
+    public float ShapeFadeDuration = 2f;
+
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,6 +15,10 @@ public class GameManager : MonoBehaviour
     private UpdateComp _updateComp;
     private int currentShapeType;
     private ScriptableObjectManager _soManager;
+
+    private float beatTipTotalTime;
+    private float generateShapeTotalTime;
+    
     
 
     private void Awake()
@@ -46,14 +50,25 @@ public class GameManager : MonoBehaviour
     private void StartOneRound()
     {
         List<int> listShapeType = new List<int>() {1, 3, 4};
-        // int type = listShapeType[Random.Range(0, listShapeType.Count)];
-        int type = listShapeType[testIndex];
+        int type = listShapeType[Random.Range(0, listShapeType.Count)];
+        // int type = listShapeType[testIndex];
         currentShapeType = type;
         testIndex = testIndex + 1 == listShapeType.Count ? 0 : testIndex + 1;
+        UpdateOneRoundTime();
         
         ShowBeatTipState();
-        _updateComp.DelayAction(GenerateShapeState, currentShapeType);
-        _updateComp.DelayAction(Test, currentShapeType + 6f);
+        _updateComp.DelayAction(GenerateShapeState, beatTipTotalTime);
+        _updateComp.DelayAction(Test, beatTipTotalTime + generateShapeTotalTime);
+    }
+    
+    private void UpdateOneRoundTime()
+    {
+        beatTipTotalTime = currentShapeType + 1f;
+        
+        float dur = SO.GetDataSettings().GenerateShapeDuration;
+        int count = SO.GetDataSettings().GenerateShapeCount;
+        float fadeDur = SO.GetDataSettings().ShapeFadeDuration;
+        generateShapeTotalTime = dur * (count - 1) + fadeDur;
     }
 
     private void ShowBeatTipState()
@@ -63,10 +78,13 @@ public class GameManager : MonoBehaviour
 
     private void GenerateShapeState()
     {
+        float dur = SO.GetDataSettings().GenerateShapeDuration;
+        int count = SO.GetDataSettings().GenerateShapeCount;
+        float fadeDur = SO.GetDataSettings().ShapeFadeDuration;
         _updateComp.ScheduleActionAndExecuteImmediately(() =>
         {
             shapeManager.GenerateOneShape();
-        }, 0.5f, 8);
+        }, dur, count);
     }
 
     private void Test()
