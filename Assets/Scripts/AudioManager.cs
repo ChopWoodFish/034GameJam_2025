17426ec36e2c7be8a0b32ad7b7801077f546e574
ChopWoodFish <cxx301320@gmail.com>
2025-03-23 17:01:09 +0800

    fix: 三角显示层级调整，bgm切换更顺滑

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
 using Util;
@@ -41,7 +42,18 @@ public class AudioManager : MonoBehaviour
         for (int i = 0; i < _listBgmAudioSource.Count; i++)
         {
             var audioSource = _listBgmAudioSource[i];
-            audioSource.mute = i != stage;
+            // audioSource.mute = i != stage;
+            // audioSource.volume = 0.8f;
+            if (i == stage)
+            {
+                DOTween.Complete(audioSource);
+                audioSource.DOFade(0.8f, 1f).SetId(audioSource);
+            }
+            else
+            {
+                DOTween.Complete(audioSource);
+                audioSource.DOFade(0f, 1f).SetId(audioSource);
+            }
         }
     }
 
@@ -52,7 +64,8 @@ public class AudioManager : MonoBehaviour
         {
             var audioSource = this.AddComponent<AudioSource>();
             audioSource.clip = dataSettings.bugStageBGM[i];
-            audioSource.mute = true;
+            // audioSource.mute = true;
+            audioSource.volume = 0f;
             audioSource.loop = true;
             audioSource.Play();
             _listBgmAudioSource.Add(audioSource);
